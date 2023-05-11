eb6c4846a52011e75151bb32af037f321d76f131
Chan Chhaya <it.chhaya@gmail.com>
2023-05-11 10:09:23 +0700

    HTTP Basic security with in memory user

---
+++ src/main/java/co/istad/mbanking/security/SecurityConfig.java
@@ -0,0 +1,76 @@
+package co.istad.mbanking.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Define in-memory user
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder().encode("123"))
+                .roles("ADMIN")
+                .build();
+        UserDetails goldUser = User.builder()
+                .username("gold")
+                .password(encoder().encode("123"))
+                .roles("ADMIN","ACCOUNT")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password(encoder().encode("123"))
+                .roles("USER")
+                .build();
+        userDetailsManager.createUser(admin);
+        userDetailsManager.createUser(goldUser);
+        userDetailsManager.createUser(user);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // Disable CSRF
+        http.csrf(csrf -> csrf.disable());
+
+        // Authorize URL mapping
+        http.authorizeHttpRequests(request -> {
+            request.requestMatchers("/api/v1/users/**").hasRole("ADMIN");
+            request.requestMatchers("/api/v1/account-types/**", "/api/v1/files/**").hasAnyRole("ACCOUNT", "USER");
+            request.anyRequest().permitAll();
+        });
+
+        // Security mechanism
+        http.httpBasic();
+
+        // Make security http STATELESS
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+}
